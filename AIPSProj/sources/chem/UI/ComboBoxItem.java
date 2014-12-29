a5d746fe0045a78c71865a360c1e3ddcc641e364
FallenShard <nemanjabartolovic@yahoo.com>
2014-12-29 23:31:04 +0100

    Another fix

--- AIPSProj/sources/chem/UI/ComboBoxItem.java
+++ AIPSProj/sources/chem/UI/ComboBoxItem.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chem.UI.util;
+
+package chem.UI;
 
 import chem.figures.persist.DocumentModel;
 import java.util.Date;
